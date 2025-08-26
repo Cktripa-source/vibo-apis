e77dc6f4be9476ba426444e1d1c8a5fa5869acd9
Cktripa-source <websdeveloper2024@gmail.com>
2025-08-25 19:42:35 -0700

    woow

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 // server.ts
 import { app } from './app.js';
-import { env } from './config/env';
+import { env } from './config/env.js';
 
 app.listen(env.PORT, () => {
   console.log(`API listening on http://localhost:${env.PORT}`);
